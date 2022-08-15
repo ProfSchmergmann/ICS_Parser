f2f3eed07b198fe54b0b609750a1c92a20a986de
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2022-08-15 18:04:37 +0200

    added test file for playing with GitHub actions

---
+++ atg_test.py
@@ -0,0 +1,5 @@
+from main import IPO
+
+
+def test_program():
+    IPO('test', 'Aachener TG', 'test')
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from game import game
 from output import write_xlsx
 
 
+def IPO(ics_file, final_home_team, xlsx_file):
+    write_xlsx(read_games(ics_file + '.ics', final_home_team), xlsx_file)
+
+
 if __name__ == '__main__':
     ics_file = input("Name of input.ics file?")
     final_home_team = input("Name of the home team?")
     xlsx_file = input("Name of output.xlsx file?")
-
-    write_xlsx(read_games(ics_file + '.ics', final_home_team), xlsx_file)
+    IPO(ics_file, final_home_team, xlsx_file)
     quit()
